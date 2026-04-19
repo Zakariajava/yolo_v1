980ccafb28225a18890dbc5223b7a5d27f032657
Zakariajava <zakariaabouhammadi@gmail.com>
2026-04-19 04:47:10 +0200

    feat: add CNNBlock module and YOLOv1 architecture config

---
+++ model.py
@@ -0,0 +1,41 @@
+import torch 
+import torch.nn as nn 
+
+# Architecture from the original paper
+# padding calculated by hand 
+architecture_config = [
+    # Kernel_size, n_filter_as_output, stride, padding
+    (7, 64, 2, 3),
+    "M",
+    (3, 192, 1, 1),
+    "M",
+    (1, 128, 1, 0),
+    (3, 256, 1, 1),
+    (1, 256, 1, 0),
+    (3, 512, 1, 1),
+    "M",
+    [(1, 256, 1, 0), (3, 512, 1, 1), 4],
+    (1, 512, 1, 0),
+    (3, 1024, 1, 1),
+    "M",
+    # parameters as before + number of repetitions 
+    [(1, 512, 1, 0), (3, 1024, 1, 1), 2],
+    (3, 1024, 1, 1),
+    (3, 1024, 2, 1),
+    (3, 1024, 1, 1),
+    (3, 1024, 1, 1),
+]
+
+
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        self.batchnorm = nn.BatchNorm2d(out_channels)
+        self.leakyrelu = nn.LeakyReLU(0.1)
+    
+    def forward(self, x):   
+        x = self.conv(x)        # apply the filters
+        x = self.batchnorm(x)   # normalize the output
+        x = self.leakyrelu(x)   # non-linearity
+        return x
