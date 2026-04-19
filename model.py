e5850dd1d0e39bb1680a86ab5cd9e1d4b080ce0b
Zakariajava <zakariaabouhammadi@gmail.com>
2026-04-19 06:11:50 +0200

    feat(model): build Yolov1 darknet backbone from architecture config

--- model.py
+++ model.py
@@ -39,3 +39,56 @@ class CNNBlock(nn.Module):
         x = self.batchnorm(x)   # normalize the output
         x = self.leakyrelu(x)   # non-linearity
         return x
+    
+class Yolov1(nn.Module):
+    def __init__(self, in_channels=3, **kwargs):
+        super().__init__()
+        self.architecture = architecture_config
+        self.in_channels = in_channels
+        self.darknet = self._create_conv_layers(self.architecture)
+        self.fcs = self._create_fcs(**kwargs)
+        
+    def forward(self, x):
+        x = self.darknet(x)
+        x = torch.flatten(x, start_dim=1) # start_dim=1 to not flatten the number of examples
+        x = self.fcs(x)
+        return x
+    
+    def _create_conv_layers(self, architecture):
+        layers = []
+        in_channels = self.in_channels
+        
+        for x in architecture:
+            if isinstance(x, tuple):
+                layers += [ 
+                    CNNBlock(
+                        in_channels, out_channels=x[1], kernel_size=x[0], stride=x[2], padding=x[3]
+                    )
+                ]
+                # storing the output channels of the previous block as input channels to be used in the next iteration
+                in_channels = x[1]
+                
+            elif isinstance(x, str):
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            
+            elif isinstance(x, list):
+                conv1 = x[0]
+                conv2 = x[1]
+                num_repeats = x[2]
+                
+                for _ in range(num_repeats):
+                    layers += [ 
+                        CNNBlock(
+                            in_channels, out_channels=conv1[1], kernel_size=conv1[0], stride=conv1[2], padding=conv1[3] 
+                        )
+                    ]
+                    
+                    layers += [ 
+                        CNNBlock(                           
+                            # the input channels of this CNNBlock is the output channels of the previous block
+                            conv1[1], out_channels=conv2[1], kernel_size=conv2[0], stride=conv2[2], padding=conv2[3] 
+                        )
+                    ]
+                # storing the output channels of the previous block as input channels to be used in the next iteration
+                in_channels = conv2[1] 
+        return nn.Sequential(*layers)
